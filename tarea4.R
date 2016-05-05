df975ff2aa4d5959bbaa9c4ef49ba21845d168b2
Miguel <miguelachof11@gmail.com>
2016-05-05 18:25:11 -0430

    listo primera parte de recomendacion

--- tarea4.R
+++ tarea4.R
@@ -1,86 +1,53 @@
-library(arules)
+library("arules")
+library("dplyr")
 
-# Transacciones
-datos <- data(Groceries)
-# inspect(Groceries) # Terrible
-summary(Groceries)
-# 1 y 2
-unique(Groceries) # el numero de columnas son la cantidad de articulos unicos
-dim(Groceries)
+setwd("C:/Users/Alex/Documents/R/DM_T4/recomendacion-modelos")
 
-# 3 y 4
-itemFrequencyPlot(Groceries, topN = 20, "absolute")
-
-# 5
-
-9835*0.0006100661
-
-# 6 169 son items unicos
-9835*169
-
-# 7 pendiente
-
-# 8 
-50/9835
-
-# 9
-reglas <- apriori(Groceries)
-# el soporte esta molestando, si el soporte es 0.1 tiene q aparecer 900 veces (10%) de las veces
-inspect(reglas)
+ejemplo <- read.csv("data/ejemplo.csv")
+periodico <- read.csv("data/periodico.csv")
 
 
-# Reglas 
-reglas <- apriori(Groceries, parameter = list(supp = 0.0006, conf = 0.98, target = "rules"))
-# dado un soporte y una confianza todos los algoritmos deberian tener los mismos resultados
-
-
-# 1
-inspect(sort(reglas, by = "support")[1:10])
-
-
-
-# 2
-inspect(sort(reglas, by = "confidence", decreasing = F)[1:10])
-
-
-# 3
-inspect(subset(reglas, subset = lhs %ain% c("sausage", 
-                                            "citrus fruit", 
-                                            "root vegetables", 
-                                            "other vegetables", 
-                                            "whole milk", 
-                                            "whipped/sour cream")))
-# 4
-inspect(subset(reglas, subset = lhs %in% c("sausage", 
-                                           "citrus fruit", 
-                                           "root vegetables", 
-                                           "other vegetables", 
-                                           "whole milk", 
-                                           "whipped/sour cream")))
-# 5
-inspect(subset(reglas, subset = lhs %in% c("pip fruit", 
-                                           "root vegetables",
-                                           "yogurt",
-                                           "soda", 
-                                           "fruit/vegetable juice")))
-
-
-# 6 
-inspect(subset(reglas, subset = lhs %in% c("whole milk",
-                                           "yogurt",
-                                           "whipped/sour cream")))
-
-unique(labels(rhs(subset(reglas, subset = lhs %in% c("whole milk",
-                                                     "yogurt",
-                                                     "whipped/sour cream"))))$elements)
+## funciones
+asignar <- function(valor){
+  #función para asignar valores
+  
+  valor <- gsub(pattern = "item", x = valor, replacement = "")
+  valor <- gsub(pattern = "}",x=valor,replacement = "")
+  valor <- gsub(pattern = "\\{",x=valor,replacement = "")
+  
+  a <- as.integer(unlist(strsplit(valor,split = ",")))
+  for(j in 1:length(a)){
+    if(j == 1){
+      h <- paste(valores[a[1]])
+    }else{
+      h <- paste(h,valores[a[j]],sep=",")
+    }
+  }
+  return(h)
+}
 
 
 
 
+#delete useless feature
+ejemplo$X <- NULL
+periodico$X <- NULL
+
+
+#creando un arreglo para asignar los valores
+contenido <- c("deporte","politica","variedades","internacional","nacionales",
+               "sucesos","comunidad","negocios","opinion")
+
+contenido <- rep(contenido,each=9) 
+
+articulo <- rep(x=1:9,times=9)
+
+articulo <- paste0("articulo",articulo,sep="")
+
+valores <- paste(contenido,articulo,sep = "/")
 
 
 
-
-
-
+items <- sapply(periodico$articles, asignar)
+periodico$items <- items
 
