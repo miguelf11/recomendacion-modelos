6eabfa12424d9f52cc68ff079f428fb8ac952ca6
Miguel <miguelachof11@gmail.com>
2016-05-16 20:31:27 -0400

    falta arreglar actividad 4 y curva ROC

--- tarea4.R
+++ tarea4.R
@@ -1,11 +1,9 @@
 library("arules")
 library("dplyr")
 library("arulesViz")
-library("reshape2")
 
 setwd("C:/Users/Alex/Documents/R/DM_T4/recomendacion-modelos")
 
-ejemplo <- read.csv("data/ejemplo.csv")
 periodico <- read.csv("data/periodico.csv")
 
 
@@ -32,33 +30,62 @@ asignar <- function(valor){
 
 
 
+### delete useless feature
+periodico$X <- NULL
+periodico$ID <- NULL
 
-#delete useless feature
-ejemplo$X <- NULL
 
 
-#creando un arreglo para asignar los valores
+# eliminar bots
+periodico$entry <- as.POSIXct(periodico$entry, origin = "1970-01-01")
+periodico$exit <- as.POSIXct(periodico$exit, origin = "1970-01-01")
+periodico <- mutate(periodico, estadia = exit - entry)
+
+length(periodico$estadia[periodico$estadia <= 20])
+
+periodico <- periodico[periodico$estadia > 20, ]
+
+
+
+# Actividad 1
+# creando un arreglo para asignar los valores
 contenido <- c("deporte","politica","variedades","internacional","nacionales",
                "sucesos","comunidad","negocios","opinion")
-
 contenido <- rep(contenido,each=9) 
-
 articulo <- rep(x=1:9,times=9)
-
 articulo <- paste0("articulo",articulo,sep="")
-
 valores <- paste(contenido,articulo,sep = "/")
-
-
-
 periodico$items <- sapply(periodico$articles, asignar)
-
 rm(articulo)
 rm(contenido)
 
 
-length(unique(periodico$X))
-length(unique(periodico$ID))
+# Crear matriz de incidencia
+aux <- periodico$items
+aux <- as.character(aux)
+aux <- gsub(pattern = "}",x=aux,replacement = "")
+aux <- gsub(pattern = "\\{",x=aux,replacement = "")
+aux <- gsub(pattern = " ",x=aux,replacement = "")
+
+# construyo la matriz de transacciones
+nuevo <- matrix(nrow=length(aux) , ncol = 81)
+nuevo[,] <- F
+nuevo <- as.data.frame(nuevo)
+colnames(nuevo) <- valores
+for (i in 1:length(aux)){
+  a <- unlist(strsplit(aux[i],split = ","))
+  print(paste("va por la: ",i))
+  for (j in a){
+    nuevo[i,j] <- T 
+  }
+}
+class(nuevo)
+# save(nuevo,file="data/nuevo.RData")
+load("data/nuevo.RData")
+
+## Actividad 3
+
+
 
 periodico$entry <- as.POSIXct(periodico$entry, origin = "1970-01-01")
 periodico$exit <- as.POSIXct(periodico$exit, origin = "1970-01-01")
@@ -80,27 +107,7 @@ estadia[1:10]
 
 
 
-aux <- periodico$items
-aux <- as.character(aux)
-aux <- gsub(pattern = "}",x=aux,replacement = "")
-aux <- gsub(pattern = "\\{",x=aux,replacement = "")
-aux <- gsub(pattern = " ",x=aux,replacement = "")
 
-aux <- aux
-
-#construyo la matriz de transacciones
-nuevo <- matrix(nrow=length(aux) , ncol = 81)
-nuevo[,] <- F
-nuevo <- as.data.frame(nuevo)
-colnames(nuevo) <- valores
-for (i in 1:length(aux)){
-  a <- unlist(strsplit(aux[i],split = ","))
-  print(paste("va por la: ",i))
-  for (j in a){
-    nuevo[i,j] <- T 
-  }
-}
-View(nuevo)
 
 
 trans <- as(nuevo, "transactions")
@@ -110,7 +117,7 @@ unique(trans)
 itemFrequencyPlot(trans, topN = 10, "absolute")
 
 
-reglas <- apriori(trans, parameter = list(supp = 0.00003, conf = 0.8, target = "rules"))
+reglas <- apriori(trans, parameter = list(supp = 0.00004, conf = 0.8, target = "rules"))
 
 
 #in es cualquiera de los 2 , %ain% son los 2
@@ -122,9 +129,27 @@ inspect(reglas)
 
 
 
+kmedias = kmeans(x = nuevo ,centers = 8)
 
 
+nuevo$kmedias <- kmedias$cluster
+
+nuevo$kmedias <- NULL
+
+periodico$kmedias <- kmedias$cluster
+
+periodico$ID <- NULL
+periodico$X <- NULL
+View(periodico)
 
 
+for (i in 1:8){
+  print(paste("la cantidad de usuarios que pertenecen al grupo:",i,": ",
+              length(periodico$kmedias[periodico$kmedias == i])))
+}
 
+for (i in 1:8){
+  print(paste("la proporción de usuarios que pertenecen al grupo:",i,": ",
+              length(periodico$kmedias[periodico$kmedias == i])/length(periodico$kmedias)))
+}
 
